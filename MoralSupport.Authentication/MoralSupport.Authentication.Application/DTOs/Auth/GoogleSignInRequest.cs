fb9bb8f8bfa8311a532a3cd74311043511b1313a
TheSchlote <TheSchlote@gmail.com>
2025-07-28 21:30:25 -0500

    working on this
    checking in for Nick to debug with me

---
+++ MoralSupport.Authentication/MoralSupport.Authentication.Application/DTOs/Auth/GoogleSignInRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace MoralSupport.Authentication.Application.DTOs.Auth
+{
+    public class GoogleSignInRequest
+    {
+        public string IdToken { get; set; } = string.Empty;
+    }
+}
---
+++ MoralSupport.Authentication/MoralSupport.Authentication.Web/Controller/AuthProviderController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MoralSupport.Authentication.Application.Interfaces;
+
+namespace MoralSupport.Authentication.Web.Controller
+{
+    [Route("api/auth/provider")]
+    [ApiController]
+    public class AuthProviderController : ControllerBase
+    {
+        private readonly IAuthService _authService;
+        public AuthProviderController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+        [HttpGet("google-client-id")]
+        public async Task<IActionResult> GetGoogleClientId()
+        {
+            var clientId = await _authService.GetGoogleClientIdAsync();
+            return Ok(new { clientId });
+        }
+    }
+}
